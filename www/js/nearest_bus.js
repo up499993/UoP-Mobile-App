33a2fb63521c7215234306ee18991af2c521df48
up499993 <Scott.walton@myport.ac.uk>
2014-03-12 18:03:28 +0000

    Find Long and Lat of Bus Stops

---
+++ www/js/nearest_bus.js
@@ -0,0 +1,49 @@
+/* Bus Stop Locations
+
+Cambridge Road (Nuffield)
+Long:-1.096767
+Lat:50.794665
+
+Winston Churchill Ave (Law Courts)
+Long:-1.092529
+Lat:50.795587
+
+Fratton Station
+Long:-1.075988
+Lat:50.796070
+
+Goldsmith Ave (Lidl)
+Long:-1.069781
+Lat:50.794974
+
+Goldsmith Ave (Milton Park)
+Long: -1.065881
+Lat:50.794535
+
+Langstone Campus (Arr)
+Long:-1.041862
+Lat:50.796143
+
+Langstone Campus (Dep)
+Long:-1.041947
+Lat:50.796061
+
+Locks Way Road (Milton)
+Long:-1.0485144
+Lat:50.7954484
+
+Goldsmith Ave (Lidl)
+Long:-1.069658
+Lat:50.794952
+
+Fratton Station
+Long:-1.073969
+Lat:50.796326
+
+Winston Churchill Ave (Ibis Hotel)
+Long:-1.0874335
+Lat:50.7945881
+
+Cambridge Road (Student Center)
+Long:-1.0961453
+Lat:50.7937268
