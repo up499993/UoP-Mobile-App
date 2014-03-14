40fae8c41503ec3a8ed088774be9f6985b69b105
up499993 <Scott.walton@myport.ac.uk>
2014-03-14 17:27:21 +0000

    Update free_pc.js
    Create object for each OA and populate with content from JSON, Give
    LAT/LONG for each OA.
    
    Get current LONG/LAT and work out distance to each OA and add to object.
    
    Sort objects by distance to get closest OA

--- www/js/free_pc.js
+++ www/js/free_pc.js
@@ -1,13 +1,124 @@
+//Define Global Variables
+var myLat;
+var myLong;
+var myPos;
+var furthestOA;
+var closestOA;
+var nearestOA;
+
 function freePC() {
 $.ajax({
     url: 'http://proj.scottwalton.co.uk/json_proxy/oa.php',
     dataType: 'json',
     success: function( oa ) {
-	console.log(oa.Library);
-	console.log(oa.Park);
-	console.log(oa.Burnaby);
-	console.log(oa.Anglesea);
-    },
+	var Library = {
+                name: 'Library',
+                status: oa.Library.status,
+                pcs: {
+                    available: oa.Library.available,
+					inuse: oa.Library.inuse,
+                    total: oa.Library.total
+                },
+                latlong: new google.maps.LatLng(50.7937, -1.0978)
+            };
+			
+	var Park = {
+                name: 'Park',
+                status: oa.Park.status,
+                pcs: {
+                    available: oa.Park.available,
+					inuse: oa.Park.inuse,
+                    total: oa.Park.total
+                },
+                latlong: new google.maps.LatLng(50.7975, -1.0940)
+            };
+			
+	var Burnaby = {
+                name: 'Burnaby',
+                status: oa.Burnaby.status,
+                pcs: {
+                    available: oa.Burnaby.available,
+					inuse: oa.Burnaby.inuse,
+                    total: oa.Burnaby.total
+                },
+                latlong: new google.maps.LatLng(50.7980, -1.0980)
+            };
+			
+	var Anglesea = {
+                name: 'Anglesea',
+                status: oa.Anglesea.status,
+                pcs: {
+                    available: oa.Anglesea.available,
+					inuse: oa.Anglesea.inuse,
+                    total: oa.Anglesea.total
+                },
+                latlong: new google.maps.LatLng(50.7978, -1.0965)
+            };
+						
+    // Get Current Location using Geolocation
+	function myLocation()
+	  {
+	  if (navigator.geolocation)
+		{
+		navigator.geolocation.getCurrentPosition(showMyPosition);
+		}
+	  else{alert("Geolocation is not supported by this browser.");}
+	  }
+	
+	// Store Current Position  
+	function showMyPosition(myPosition)
+	{
+	  myLat = myPosition.coords.latitude;
+	  myLong = myPosition.coords.longitude;
+	  myPos = [myPosition.coords.latitude,myPosition.coords.longitude];
+	  myPos_G = new google.maps.LatLng(myLat, myLong);
+	  	  
+	  // Call Distance To OA Function
+	  function distanceToOA()
+	  {
+	  // Find Distance to Each Stop from Current Position
+	  Library.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Library.latlong);
+	  Park.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Park.latlong);
+	  Burnaby.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Burnaby.latlong);
+	  Anglesea.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Anglesea.latlong);
+	   console.log(Library);
+	   console.log(Park);
+	   console.log(Burnaby);
+	   console.log(Anglesea);
+	 
+	  // Put Distance to each stop into an array and find shortest distance.
+	  distToOA = [Library, Park, Burnaby, Anglesea];
+	  distToOA.sort(function(a, b)
+	{
+		return a.dist - b.dist;
+	});
+	  
+	  //furthestOA = Array.max(distToOA);
+	closestOA = distToOA.sort(function(a, b)
+	{
+		return a.dist - b.dist;
+	})[0][1];
+
+	console.log(distToOA.sort(function(a, b)
+	{
+		return a.dist - b.dist;
+	})[0].name);
+	  
+	  
+	  
+	  // Display Nearest OA On Page
+	  //document.querySelector('#nearestOA').innerHTML = nearestOA;
+	  
+	  
+	  
+	  } 
+	  
+	  
+	 distanceToOA(); 
+	}
+	myLocation();
+	},
+	//If JSON Feed doesn't load run this instead to use dummy data
     error: function( oa ) {
     var jsonOA = generateDummyData();
 	var oa = JSON.parse(jsonOA);
@@ -18,15 +129,7 @@ $.ajax({
 
     }
   });
-
-
-//Find Closest OA
-
-/* OA Lat Long
-
-
-*/
-
+  
  }
  
 // Generate Dummy JSON for testing
