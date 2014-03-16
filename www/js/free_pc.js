08567454764b4acb6e26c649a15d6fd03fb8964e
up499993 <Scott.walton@myport.ac.uk>
2014-03-16 02:16:00 +0000

    Update free_pc.js
    add percentage bars and output to page

--- www/js/free_pc.js
+++ www/js/free_pc.js
@@ -5,14 +5,19 @@ var myPos;
 var furthestOA;
 var closestOA;
 var nearestOA;
+var nearestPC;
 
 //status 0 is closed, status 1 is almost full, status 2 is available
 
 function freePC() {
+//Read JSON Feed
+console.log("Fetch JSON");
 $.ajax({
     url: 'http://proj.scottwalton.co.uk/json_proxy/oa.php',
     dataType: 'json',
     success: function( oa ) {
+	console.log("Got JSON");
+	//Create Object for each OA
 	var Library = {
                 name: 'Library',
                 status: oa.Library.status,
@@ -56,7 +61,7 @@ $.ajax({
                 },
                 latlong: new google.maps.LatLng(50.7978, -1.0965)
             };
-						
+	var allOA = [Library, Park, Burnaby, Anglesea];					
     // Get Current Location using Geolocation
 	function myLocation()
 	  {
@@ -70,6 +75,7 @@ $.ajax({
 	// Store Current Position  
 	function showMyPosition(myPosition)
 	{
+	  console.log("Show Position");
 	  myLat = myPosition.coords.latitude;
 	  myLong = myPosition.coords.longitude;
 	  myPos = [myPosition.coords.latitude,myPosition.coords.longitude];
@@ -78,6 +84,7 @@ $.ajax({
 	  // Call Distance To OA Function
 	  function distanceToOA()
 	  {
+	  console.log("Get Distances");
 	  // Find Distance to Each Stop from Current Position
 	  Library.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Library.latlong);
 	  Park.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Park.latlong);
@@ -90,10 +97,8 @@ $.ajax({
 	 
 	  // Put Distance to each stop into an array and find shortest distance.
 	  distToOA = [Library, Park, Burnaby, Anglesea];
-	  distToOA.sort(function(a, b)
-	{
-		return a.dist - b.dist;
-	});
+	  console.log("Sort");
+	  
 	  
 	  //furthestOA = Array.max(distToOA);
 	closestOA = distToOA.sort(function(a, b)
@@ -101,8 +106,9 @@ $.ajax({
 		return a.dist - b.dist;
 	})[0][1];
 
-		
+	console.log("Sorted");	
 	//Check to see if open
+	console.log("Check if open");
 	if (distToOA.sort(function(a, b){return a.dist - b.dist;})[0].status === 0)
 	{
 		distToOA.sort(function(a, b){return a.dist - b.dist;})[0].pcs.available = 0;
@@ -119,8 +125,16 @@ $.ajax({
 	{
 		distToOA.sort(function(a, b){return a.dist - b.dist;})[3].pcs.available = 0;
 	}
+	console.log("Checked");
+	// Get Percentage Free PCs for each OA
+	console.log("Calculate percentages");
+	for (var i = 0; i < allOA.length; i++) {
+		allOA[i].pcs.percentFree = Math.floor((allOA[i].pcs.available / allOA[i].pcs.total) * 100);
+	}
+	console.log("Done!");
 	
 	// Check if any free PCs
+	console.log("Any Free PCs?");
 	if (distToOA.sort(function(a, b){return a.dist - b.dist;})[0].pcs.available < 1) 
 	{
 		if (distToOA.sort(function(a, b){return a.dist - b.dist;})[1].pcs.available < 1) 
@@ -133,46 +147,303 @@ $.ajax({
 				}
 				else
 				{
-					console.log(distToOA.sort(function(a, b){return a.dist - b.dist;})[3].name);
+					nearestPC = distToOA.sort(function(a, b){return a.dist - b.dist;})[3];
 				}
 			}
 			else
 			{
-				console.log(distToOA.sort(function(a, b){return a.dist - b.dist;})[2].name);
+				nearestPC = distToOA.sort(function(a, b){return a.dist - b.dist;})[2];
 			}
 		}
 		else
 		{
-			console.log(distToOA.sort(function(a, b){return a.dist - b.dist;})[1].name);
+			nearestPC = distToOA.sort(function(a, b){return a.dist - b.dist;})[1];
 		}
 	}
 	else
 	{
-		console.log(distToOA.sort(function(a, b){return a.dist - b.dist;})[0].name);
+		nearestPC = distToOA.sort(function(a, b){return a.dist - b.dist;})[1];		
 	}
+	console.log("Done");	
+	function percentageBarColour(percentage) {
+	    var red = 0,
+            green = 0;
+        if (percentage >= 50) {
+            red = 255 - Math.round(((percentage - 50) / 50) * 255);
+            green = 255;
+        } else {
+            red = 255;
+            green = Math.round(((percentage) / 50) * 255);
+
+        }
+        var colour =  "rgb(" + red + "," + green + ",0)";
 		
-		
+		return colour;
+    }	
+	
 	  // Display Nearest OA On Page
-	  //document.querySelector('#nearestOA').innerHTML = nearestOA;
-	  
-	  
+	  document.querySelector('#OA-Nearest-Name').innerHTML = nearestPC.name;
+	  var percentageBarColour = percentageBarColour(nearestPC.pcs.percentFree);
+	  document.querySelector('#OA-Nearest-Percent').style.background=percentageBarColour;
+	  document.querySelector('#OA-Nearest-Percent').style.width=nearestPC.pcs.percentFree + "%";
+	  document.querySelector('#OA-Nearest-Info').innerHTML = "Open: " + nearestPC.pcs.available + " out of " + nearestPC.pcs.total + " PCs available";
+	
+	
+	  // Display All OA On Page
+	  for (var i = 0; i < distToOA.sort(function(a, b){return a.dist - b.dist;}).length; i++) {
+		
+		document.querySelector('#OA-' + i + '-Name').innerHTML = distToOA.sort(function(a, b){return a.dist - b.dist;})[i].name;
+		
+		if (distToOA.sort(function(a, b){return a.dist - b.dist;})[i].status === 0) {
+		openClose = "Closed: ";
+		}
+		else {
+		openClose = "Open: ";
+		}
+		
+		var percentage = parseInt(Math.floor((distToOA.sort(function(a, b){return a.dist - b.dist;})[i].pcs.available / distToOA.sort(function(a, b){return a.dist - b.dist;})[i].pcs.total) * 100));
+		
+		var red = 0,
+            green = 0;
+        if (percentage >= 50) {
+            red = 255 - Math.round(((percentage - 50) / 50) * 255);
+            green = 255;
+        } else {
+            red = 255;
+            green = Math.round(((percentage) / 50) * 255);
+
+        }
+        var percentageBarColour =  "rgb(" + red + "," + green + ",0)";
+		
+		document.querySelector('#OA-' + i + '-Percent').style.background=percentageBarColour;
+		document.querySelector('#OA-' + i + '-Percent').style.width=percentage + "%";
+				
+		document.querySelector('#OA-' + i + '-Info').innerHTML = openClose + distToOA.sort(function(a, b){return a.dist - b.dist;})[i].pcs.available + " out of " + distToOA.sort(function(a, b){return a.dist - b.dist;})[i].pcs.total + " PCs available";
+	}
 	  
 	  } 
 	  
 	  
 	 distanceToOA(); 
 	}
+	console.log("Get Location");
 	myLocation();
 	},
 	//If JSON Feed doesn't load run this instead to use dummy data
     error: function( oa ) {
-    var jsonOA = generateDummyData();
-	var oa = JSON.parse(jsonOA);
-	console.log(oa.Library);
-	console.log(oa.Park);
-	console.log(oa.Burnaby);
-	console.log(oa.Anglesea);
+	console.log("DUMMY DATA");
+    console.log("Got JSON");
+	//Create Object for each OA
+	var Library = {
+                name: 'Library',
+                status: oa.Library.status,
+                pcs: {
+                    available: oa.Library.available,
+					inuse: oa.Library.inuse,
+                    total: oa.Library.total
+                },
+                latlong: new google.maps.LatLng(50.7937, -1.0978)
+            };
+			
+	var Park = {
+                name: 'Park',
+                status: oa.Park.status,
+                pcs: {
+                    available: oa.Park.available,
+					inuse: oa.Park.inuse,
+                    total: oa.Park.total
+                },
+                latlong: new google.maps.LatLng(50.7975, -1.0940)
+            };
+			
+	var Burnaby = {
+                name: 'Burnaby',
+                status: oa.Burnaby.status,
+                pcs: {
+                    available: oa.Burnaby.available,
+					inuse: oa.Burnaby.inuse,
+                    total: oa.Burnaby.total
+                },
+                latlong: new google.maps.LatLng(50.7980, -1.0980)
+            };
+			
+	var Anglesea = {
+                name: 'Anglesea',
+                status: oa.Anglesea.status,
+                pcs: {
+                    available: oa.Anglesea.available,
+					inuse: oa.Anglesea.inuse,
+                    total: oa.Anglesea.total
+                },
+                latlong: new google.maps.LatLng(50.7978, -1.0965)
+            };
+	var allOA = [Library, Park, Burnaby, Anglesea];					
+    // Get Current Location using Geolocation
+	function myLocation()
+	  {
+	  if (navigator.geolocation)
+		{
+		navigator.geolocation.getCurrentPosition(showMyPosition);
+		}
+	  else{alert("Geolocation is not supported by this browser.");}
+	  }
+	
+	// Store Current Position  
+	function showMyPosition(myPosition)
+	{
+	  console.log("Show Position");
+	  myLat = myPosition.coords.latitude;
+	  myLong = myPosition.coords.longitude;
+	  myPos = [myPosition.coords.latitude,myPosition.coords.longitude];
+	  myPos_G = new google.maps.LatLng(myLat, myLong);
+	  	  
+	  // Call Distance To OA Function
+	  function distanceToOA()
+	  {
+	  console.log("Get Distances");
+	  // Find Distance to Each Stop from Current Position
+	  Library.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Library.latlong);
+	  Park.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Park.latlong);
+	  Burnaby.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Burnaby.latlong);
+	  Anglesea.dist = google.maps.geometry.spherical.computeDistanceBetween(myPos_G, Anglesea.latlong);
+	   console.log(Library);
+	   console.log(Park);
+	   console.log(Burnaby);
+	   console.log(Anglesea);
+	 
+	  // Put Distance to each stop into an array and find shortest distance.
+	  distToOA = [Library, Park, Burnaby, Anglesea];
+	  console.log("Sort");
+	  
+	  
+	  //furthestOA = Array.max(distToOA);
+	closestOA = distToOA.sort(function(a, b)
+	{
+		return a.dist - b.dist;
+	})[0][1];
 
+	console.log("Sorted");	
+	//Check to see if open
+	console.log("Check if open");
+	if (distToOA.sort(function(a, b){return a.dist - b.dist;})[0].status === 0)
+	{
+		distToOA.sort(function(a, b){return a.dist - b.dist;})[0].pcs.available = 0;
+	}
+	if (distToOA.sort(function(a, b){return a.dist - b.dist;})[1].status === 0)
+	{
+		distToOA.sort(function(a, b){return a.dist - b.dist;})[1].pcs.available = 0;
+	}
+	if (distToOA.sort(function(a, b){return a.dist - b.dist;})[2].status === 0)
+	{
+		distToOA.sort(function(a, b){return a.dist - b.dist;})[2].pcs.available = 0;
+	}
+	if (distToOA.sort(function(a, b){return a.dist - b.dist;})[3].status === 0)
+	{
+		distToOA.sort(function(a, b){return a.dist - b.dist;})[3].pcs.available = 0;
+	}
+	console.log("Checked");
+	// Get Percentage Free PCs for each OA
+	console.log("Calculate percentages");
+	for (var i = 0; i < allOA.length; i++) {
+		allOA[i].pcs.percentFree = Math.floor((allOA[i].pcs.available / allOA[i].pcs.total) * 100);
+	}
+	console.log("Done!");
+	
+	// Check if any free PCs
+	console.log("Any Free PCs?");
+	if (distToOA.sort(function(a, b){return a.dist - b.dist;})[0].pcs.available < 1) 
+	{
+		if (distToOA.sort(function(a, b){return a.dist - b.dist;})[1].pcs.available < 1) 
+		{
+			if (distToOA.sort(function(a, b){return a.dist - b.dist;})[2].pcs.available < 1) 
+			{
+				if (distToOA.sort(function(a, b){return a.dist - b.dist;})[3].pcs.available < 1) 
+				{
+					console.log("No Free PCs");
+				}
+				else
+				{
+					nearestPC = distToOA.sort(function(a, b){return a.dist - b.dist;})[3];
+				}
+			}
+			else
+			{
+				nearestPC = distToOA.sort(function(a, b){return a.dist - b.dist;})[2];
+			}
+		}
+		else
+		{
+			nearestPC = distToOA.sort(function(a, b){return a.dist - b.dist;})[1];
+		}
+	}
+	else
+	{
+		nearestPC = distToOA.sort(function(a, b){return a.dist - b.dist;})[1];		
+	}
+	console.log("Done");	
+	function percentageBarColour(percentage) {
+	    var red = 0,
+            green = 0;
+        if (percentage >= 50) {
+            red = 255 - Math.round(((percentage - 50) / 50) * 255);
+            green = 255;
+        } else {
+            red = 255;
+            green = Math.round(((percentage) / 50) * 255);
+
+        }
+        var colour =  "rgb(" + red + "," + green + ",0)";
+		
+		return colour;
+    }	
+	
+	  // Display Nearest OA On Page
+	  document.querySelector('#OA-Nearest-Name').innerHTML = nearestPC.name;
+	  var percentageBarColour = percentageBarColour(nearestPC.pcs.percentFree);
+	  document.querySelector('#OA-Nearest-Percent').style.background=percentageBarColour;
+	  document.querySelector('#OA-Nearest-Percent').style.width=nearestPC.pcs.percentFree + "%";
+	  document.querySelector('#OA-Nearest-Info').innerHTML = "Open: " + nearestPC.pcs.available + " out of " + nearestPC.pcs.total + " PCs available";
+	
+	
+	  // Display All OA On Page
+	  for (var i = 0; i < distToOA.sort(function(a, b){return a.dist - b.dist;}).length; i++) {
+		
+		document.querySelector('#OA-' + i + '-Name').innerHTML = distToOA.sort(function(a, b){return a.dist - b.dist;})[i].name;
+		
+		if (distToOA.sort(function(a, b){return a.dist - b.dist;})[i].status === 0) {
+		openClose = "Closed: ";
+		}
+		else {
+		openClose = "Open: ";
+		}
+		
+		var percentage = parseInt(Math.floor((distToOA.sort(function(a, b){return a.dist - b.dist;})[i].pcs.available / distToOA.sort(function(a, b){return a.dist - b.dist;})[i].pcs.total) * 100));
+		
+		var red = 0,
+            green = 0;
+        if (percentage >= 50) {
+            red = 255 - Math.round(((percentage - 50) / 50) * 255);
+            green = 255;
+        } else {
+            red = 255;
+            green = Math.round(((percentage) / 50) * 255);
+
+        }
+        var percentageBarColour =  "rgb(" + red + "," + green + ",0)";
+		
+		document.querySelector('#OA-' + i + '-Percent').style.background=percentageBarColour;
+		document.querySelector('#OA-' + i + '-Percent').style.width=percentage + "%";
+				
+		document.querySelector('#OA-' + i + '-Info').innerHTML = openClose + distToOA.sort(function(a, b){return a.dist - b.dist;})[i].pcs.available + " out of " + distToOA.sort(function(a, b){return a.dist - b.dist;})[i].pcs.total + " PCs available";
+	}
+	  
+	  } 
+	  
+	  
+	 distanceToOA(); 
+	}
+	console.log("Get Location");
+	myLocation();
     }
   });
   
