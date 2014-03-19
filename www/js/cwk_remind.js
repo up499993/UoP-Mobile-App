bb34a3cdd0d7c8ee1ea5f736bb84a4249925fc40
up499993 <Scott.walton@myport.ac.uk>
2014-03-19 14:58:32 +0000

    Local Notify Changes
    Add Remove methods

--- www/js/cwk_remind.js
+++ www/js/cwk_remind.js
@@ -1,14 +1,29 @@
 function cwkRemind()
 {
+}
+
+function cwkRemindCancelAll()
+{
+window.plugin.notification.local.cancelAll();
+}
+
+function cwkRemindCancel(id)
+{
+window.plugin.notification.local.cancel(id);
+}
+
+function cwkRemindAdd ()
+{
+
 var now                  = new Date().getTime(),
-    _60_seconds_from_now = new Date(now + 60*1000);
+    _05_seconds_from_now = new Date(now + 05*1000);
 
 window.plugin.notification.local.add({
     id:         1, // is converted to a string
     title:      'Reminder',
     message:    'Dont forget to buy some flowers.',
     repeat:     'weekly',
-    date:       _60_seconds_from_now,
+    date:       _05_seconds_from_now,
     foreground: 'foreground',
     background: 'background'
 });
