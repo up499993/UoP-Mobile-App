14efbd823d32921768fa18ca3556c1884ccae651
up499993 <Scott.walton@myport.ac.uk>
2014-03-19 10:55:02 +0000

    Add cwk_remind.js

---
+++ www/js/cwk_remind.js
@@ -0,0 +1,6 @@
+function cwkRemind()
+{
+window.addcalendareventplugin(function(val){
+        alert(val);   //once success message come and you have tested it, you can remove this alert.
+    });
+}
