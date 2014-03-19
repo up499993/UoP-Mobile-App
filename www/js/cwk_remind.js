0a07a10a5711cd3ec1cbcc11c120261e0b95e1f5
up499993 <Scott.walton@myport.ac.uk>
2014-03-19 14:40:50 +0000

    Updates to LocalNotification test

--- www/js/cwk_remind.js
+++ www/js/cwk_remind.js
@@ -1,37 +1,19 @@
 function cwkRemind()
 {
-window.plugins.localNotification.add({
-    fireDate        : Math.round(new Date().getTime()/1000 + 5),
-    alertBody       : "This is a local notification.",
-    action          : "View",
-    repeatInterval  : "daily",
-    soundName       : "beep.caf",
-    badge           : 0,
-    notificationId  : 123,
-    foreground      : function(notificationId){ 
-        alert("Hello World! This alert was triggered by notification " + notificationId); 
-    },
-    background  : function(notificationId){
-        alert("Hello World! This alert was triggered by notification " + notificationId);
-    }           
-});
+alert("Manual01"); 
+var d = new Date(); 
+d = d.getTime() + 60*1000; //60 seconds from now 
+d = new Date(d); 
 
-		  /*
+alert("Manual02"); 
+window.plugins.localNotification.add({ 
+date: d, // your set date object 
+message: 'Hello world!', 
+repeat: 'weekly', // will fire every week on this day 
+badge: 0, 
+foreground:'foreground', 
+background:'background' 
+}); 
 
-
- // prep some variables
-  var startDate = new Date(2014,2,15,18,30,0,0,0); // beware: month 0 = january, 11 = december
-  var endDate = new Date(2014,2,15,19,30,0,0,0);
-  var title = "My nice event";
-  var location = "Home";
-  var notes = "Some notes about this event.";
-  var success = function(message) { alert("Success: " + JSON.stringify(message)); };
-  var error = function(message) { alert("Error: " + message); };
-  
-  window.plugins.calendar.createEventInteractively(title,location,notes,startDate,endDate,success,error);
-
-window.addcalendareventplugin(function(val){
-        alert(val);   //once success message come and you have tested it, you can remove this alert.
-    });
-	*/
+alert("Manual03"); 
 }
