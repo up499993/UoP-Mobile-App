e64bf0070958e6ab42826e19a22647291add72fd
up499993 <Scott.walton@myport.ac.uk>
2014-03-19 17:49:34 +0000

    Update cwk_remind.js
    build functions for cwk reminder

--- www/js/cwk_remind.js
+++ www/js/cwk_remind.js
@@ -1,14 +1,31 @@
-function cwkRemind()
+function loadCwkReminders()
 {
-//var cwkNotifications = [];
-//cwkNotifications[0] = message;
-//localStorage["cwkNotifications"] = JSON.stringify(cwkNotifications);
+// Check to see if variable exists and create if necessary
+if (typeof cwkNotifications === 'undefined') {
+    // variable is undefined
+	var cwkNotifications = [];
+}
 
-//...
-alert("Reading");
-var cwkNotificationsStorred = JSON.parse(localStorage["cwkNotifications"]);
-
-alert(cwkNotificationsStorred);
+// Check to see if localStorage key exists
+if (localStorage.getItem("cwkNotifications") === null) {
+  localStorage["cwkNotifications"] = JSON.stringify(cwkNotifications);
+  document.querySelector('#cwkReminders').innerHTML = "There are no Coursework Reminders to Display";
+}
+else
+{
+var cwkNotifications = JSON.parse(localStorage["cwkNotifications"]);
+console.log (cwkNotifications.length);
+if (cwkNotifications.length === 0)
+{
+}
+else
+{
+for (var i = 0; i < cwkNotifications.length; i++) {
+	document.querySelector('#cwkReminders').innerHTML = document.querySelector('#cwkReminders').innerHTML + "<div id='notification" + i + "'><p class='bold'>" + cwkNotifications[i].module + "</p>" + "<p>" + cwkNotifications[i].desc + "</p>" + "<p><span class='bold'>Notes:</span> " + cwkNotifications[i].notes + "</p><button onClick='cwkRemindCancel(" + i + ");'>Delete</button><hr /></div>";
+   
+}
+}
+}
 }
 
 function cwkRemindCancelAll()
@@ -20,7 +37,7 @@ alert("Removed all Notifications");
 function cwkRemindCancel(id)
 {
 window.plugin.notification.local.cancel(id);
-alert("Removed Notification");
+alert("Removed Reminder");
 }
 
 function cwkRemindAdd ()
@@ -56,4 +73,31 @@ function foreground (id) {
 function background (id) {
     console.log('I WAS IN THE BACKGROUND ID='+id)
 }
+}
+
+//  T E S T I N G
+function test()
+{
+alert("Local Storage Contains " + cwkNotifications.length + " items");
+}
+var cwkModule = $('#cwkModule').val();
+var cwkDesc = $('#cwkDesc').val();
+var cwkNotes = $('#cwkNotes').val();
+
+var message = cwkModule + ": " + cwkDesc + ". " + cwkNotes;
+var arrayPos = cwkNotifications.length;
+alert("New Position: " + arrayPos);
+//var cwkNotifications = [];
+var addToArray = {
+                notificationNo: arrayPos,
+                message: message,
+				module: cwkModule,
+				desc: cwkDesc,
+				notes: cwkNotes
+            };
+//var addToArray = [arrayPos, message];
+alert("Write to Local Storage");
+cwkNotifications.push(addToArray);
+console.log(cwkNotifications);
+localStorage["cwkNotifications"] = JSON.stringify(cwkNotifications);
 }
