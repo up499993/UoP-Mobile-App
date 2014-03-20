fa1f96375b4a0c0ab1f4a65c3cb7550648f7e99c
up499993 <Scott.walton@myport.ac.uk>
2014-03-20 11:36:01 +0000

    Update cwk_remind.js

--- www/js/cwk_remind.js
+++ www/js/cwk_remind.js
@@ -57,48 +57,11 @@ console.log("localstorage: " + localStorage.cwkNotifications);
 
 window.plugin.notification.local.cancel(id);
 alert("Removed Reminder");
-
+loadCwkReminders();
 }
 
 function cwkRemindAdd ()
-{
-var cwkModule = $('#cwkModule').val();
-var cwkDesc = $('#cwkDesc').val();
-
-var message = cwkModule + ": " + cwkDesc;
-
-var cwkNotifications = [];
-cwkNotifications[0] = message;
-localStorage["cwkNotifications"] = JSON.stringify(cwkNotifications);
-
-var now                  = new Date().getTime(),
-    _05_seconds_from_now = new Date(now + 05*1000);
-
-window.plugin.notification.local.add({
-    id:         1, // is converted to a string
-    title:      'Reminder',
-    message:    message,
-    repeat:     'weekly',
-    date:       _05_seconds_from_now,
-    foreground: 'foreground',
-    background: 'background'
-
-
-});
-cwkRemind();
-function foreground (id) {
-    console.log('I WAS RUNNING ID='+id)
-}
-
-function background (id) {
-    console.log('I WAS IN THE BACKGROUND ID='+id)
-}
-}
-
-//  T E S T I N G
-function test()
-{
-var cwkNotifications = JSON.parse(localStorage["cwkNotifications"]);
+{var cwkNotifications = JSON.parse(localStorage["cwkNotifications"]);
 alert("Local Storage Contains " + cwkNotifications.length + " items");
 
 var cwkModule = $('#cwkModule').val();
@@ -147,5 +110,11 @@ function foreground (id) {
 function background (id) {
     console.log('I WAS IN THE BACKGROUND ID='+id)
 }
+}
+
+//  T E S T I N G
+function test()
+{
+
 
 }
