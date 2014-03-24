5ea381476c0bddc21610b865400eb85cdb90ed37
up499993 <Scott.walton@myport.ac.uk>
2014-03-24 15:21:12 +0000

    Update directory.js

--- www/js/directory.js
+++ www/js/directory.js
@@ -3,6 +3,6 @@ function directory() {
          var ref = window.open('http://sdtools.port.ac.uk/directory8', '_blank', 'location=yes', 'hidden=yes');
          //ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
          //ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
-         ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+         ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message + event.code); });
          ref.addEventListener('exit', function(event) { alert(event.type); });
     }
