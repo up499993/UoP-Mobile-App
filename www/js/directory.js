c99917a57454dc98666550db19e6e115c3d1fb19
up499993 <Scott.walton@myport.ac.uk>
2014-03-24 14:49:03 +0000

    Changes to inappbrowser settings

--- www/js/directory.js
+++ www/js/directory.js
@@ -1,6 +1,6 @@
 function directory() {
 		alert("go");
-         var ref = window.open('http://sdtools.port.ac.uk/directory', '_blank', 'location=yes');
+         var ref = window.open('http://sdtools.port.ac.uk/directory', '_blank', 'location=yes', 'hidden=yes');
          ref.addEventListener('loadstart', function(event) { alert('start: ' + event.url); });
          ref.addEventListener('loadstop', function(event) { alert('stop: ' + event.url); });
          ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
