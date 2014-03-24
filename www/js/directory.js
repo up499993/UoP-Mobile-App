1940ec37fc3d5eff939ae416de5e85ededca55d3
up499993 <Scott.walton@myport.ac.uk>
2014-03-24 16:42:33 +0000

    Update directory.js

--- www/js/directory.js
+++ www/js/directory.js
@@ -3,10 +3,10 @@ function directory() {
 	alert(cli_ip);
 	if (cli_ip === '148.197')
 	{
-		var ref = window.open('http://sdtools.port.ac.uk/directory8', '_blank', 'location=yes');         
+		var ref = window.open('http://sdtools.port.ac.uk/directory', '_blank', 'location=yes');         
     }
 	else
 	{
-	alert("Only Available when on campus");
+	var ref = window.open('main.html#directory', '_self');
 	}
 }
